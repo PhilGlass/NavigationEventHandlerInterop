42c9157f2816d85abb39dfba5ccee742ed64f1d6
Phil Glass <philglass@monzo.com>
2025-06-05 15:48:13 +0100

    Demo broken BackHandler interop

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.navigationeventhandlerordering"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.navigationeventhandlerordering"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation("androidx.activity:activity-compose:1.12.0-alpha02")
+    implementation("androidx.navigationevent:navigationevent:1.0.0-alpha02")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha15")
+}
---
+++ app/src/main/java/com/example/navigationeventhandlerordering/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.navigationeventhandlerordering
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            if (false) { // Change to false to demo a parent NavigationEventHandler
+                BackHandler {
+                    // ❌ This takes precedence over the nested NavigationEventHandler.
+                    toast("Parent BackHandler called")
+                }
+            } else {
+                NavigationEventHandler {
+                    // ✅ This does not take precedence over the nested NavigationEventHandler,
+                    // unless the nested handler is disabled.
+                    it.collect { /* ignore in-progress back gestures for the purposes of this demo */ }
+                    toast("Parent NavigationEventHandler called")
+                }
+            }
+
+            NestedNavigationEventHandler()
+        }
+    }
+
+    @Composable
+    fun NestedNavigationEventHandler() {
+        NavigationEventHandler(enabled = { true }) {
+            it.collect { /* ignore in-progress back gestures for the purposes of this demo */ }
+            toast("Child NavigationEventHandler called")
+        }
+    }
+
+    fun toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
